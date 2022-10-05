0aad9b70971b42f02f75298815058763f3a834be
Cryxyemi <113242324+Cryxyemi@users.noreply.github.com>
2022-10-05 20:00:57 +0200

    Add files via upload

---
+++ Install requirements.bat
@@ -0,0 +1,7 @@
+title Installing requirements
+
+py -3 -m pip install -U py-cord
+py -3 -m pip install -U pyttsx3
+py -3 -m pip install -U pystyle==0.6
+py -3 -m pip install -U httpx
+py -3 -m pip install -U psutil
